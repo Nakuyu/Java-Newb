a2fbf2883190e7f3673926ea12a2f53a406fdf25
Nakuyu <118659788+Nakuyu@users.noreply.github.com>
2022-11-20 17:23:21 +0530

    Knowing how to use git.

--- Day_1.java
+++ Day_1.java
@@ -1,6 +1,5 @@
 public class Day_1 {
     public static void main(String[] args) {
-        System.out.println("Hello Deep, You Stupid Person !!");
+        System.out.println("Hello Deep, YOU KNOW HOW TO USE GIT !");
     }
-
 }
