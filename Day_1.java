61f9f688beb1c636abc363da49d9142f5fc133d0
Nakuyu <118659788+Nakuyu@users.noreply.github.com>
2022-11-20 17:32:41 +0530

    Adding an Untracked file Day1.1 and Modifying Day1

--- Day_1.java
+++ Day_1.java
@@ -1,5 +1,5 @@
 public class Day_1 {
     public static void main(String[] args) {
-        System.out.println("Hello Deep, YOU KNOW HOW TO USE GIT !");
+        System.out.println("Hello Deep, Still a Stupid !");
     }
 }
