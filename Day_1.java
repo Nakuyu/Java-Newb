4c97d88796c9c548e57978c586d66afbb61ae1b5
Nakuyu <118659788+Nakuyu@users.noreply.github.com>
2022-11-25 13:02:46 +0530

    Update Day_1.java

--- Day_1.java
+++ Day_1.java
@@ -1,6 +1,7 @@
 public class Day_1 {
     public static void main(String[] args) {
-        System.out.println(args[1]);
-        
+        int a = 10;
+    
+        System.out.println(a);
     }
 }
