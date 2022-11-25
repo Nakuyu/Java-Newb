23b369fdb75014200fd1dc71c30ae9073de49132
Nakuyu <118659788+Nakuyu@users.noreply.github.com>
2022-11-25 18:35:40 +0530

    update its actually day 2 now

--- Day_1.java
+++ Day_1.java
@@ -1,3 +1,5 @@
+// java.util.Scanner;
+
 public class Day_1 {
     public static void main(String[] args) {
         int a = 10;
@@ -5,9 +7,11 @@ public class Day_1 {
         int sum = a + b;
         int diff = b - a;
         int mul = a*b;
+        int question = (a*b)/(b-a);
 
         System.out.println(sum);
         System.out.println(diff);
         System.out.println(mul);
+        System.out.println(question);
     }
 }
