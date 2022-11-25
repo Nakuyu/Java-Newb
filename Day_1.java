a2ddfaf11453e3436c6ddd0276675f780649937b
Nakuyu <118659788+Nakuyu@users.noreply.github.com>
2022-11-25 13:18:13 +0530

    new day learning variables.2
    owo

--- Day_1.java
+++ Day_1.java
@@ -1,7 +1,13 @@
 public class Day_1 {
     public static void main(String[] args) {
         int a = 10;
-    
-        System.out.println(a);
+        int b = 43;
+        int sum = a + b;
+        int diff = b - a;
+        int mul = a*b;
+
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(mul);
     }
 }
