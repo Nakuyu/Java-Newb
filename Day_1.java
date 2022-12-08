0c52e51777d81c1533e9afee6b2920e61db8d3cc
Nakuyu <118659788+Nakuyu@users.noreply.github.com>
2022-12-08 09:05:19 +0530

    owO

--- Day_1.java
+++ Day_1.java
@@ -2,14 +2,19 @@ import java.util.*;
 
 public class Day_1 {
     public static void main(String[] args) {
+        System.out.println("Enter two numbers:");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
         
+        if(a>b&&a>c) {
+            System.out.println("a is greater than b and c");
 
-        if(a%2==0) {
-            System.out.println("a is even number");
-        } else {
-            System.out.println("a is odd");
+        } else if(b>a&&b>c) {
+            System.out.println("b id greater than a and c");
+        } else if(c>a&&c>b) {
+            System.out.println("c is greater than a and b");
         }
 
     }
