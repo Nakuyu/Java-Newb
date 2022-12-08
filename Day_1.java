cf5fac48ee9973dd68c8f5e0d94ba7ee2a10ad12
Nakuyu <118659788+Nakuyu@users.noreply.github.com>
2022-12-08 20:53:54 +0530

    oof
    college is teaching me c so i m using the same statements to do them in java.

--- Day_1.java
+++ Day_1.java
@@ -2,17 +2,25 @@ import java.util.*;
 
 public class Day_1 {
     public static void main(String[] args) {
-        System.out.println("Enter choice from 1 to 2");
+        System.out.println("1.Addition\n");
+        System.out.println("2.Subtraction\n");
+        System.out.println("3.Multiplication\n");
+        System.out.println("4.Division\n");
         Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
-        //int b = sc.nextInt();
-        //int c = sc.nextInt();
-        switch(choice) {
-            case 1 : System.out.println("hellow deep");
-            break;
-            case 2 : System.out.println("heelo prATIK");
-            break;
-        }
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
+        switch(choice) {
+            case 1 : System.out.printf("Summation %d \n",a+b);
+            break;
+            case 2 : System.out.printf("Subraction %d",a-b);
+            break;
+            case 3 : System.out.printf("Multiplication %d",a*b);
+            break;
+            case 4 : System.out.printf("Division %d",a/b);
+            break;
+            default : System.out.printf("invalid");
+        }
     }
 }
