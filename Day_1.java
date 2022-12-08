8feae9c0882ba80fd224c02cae48d1e91adc1dfb
Nakuyu <118659788+Nakuyu@users.noreply.github.com>
2022-12-08 09:35:36 +0530

    Update Day_1.java

--- Day_1.java
+++ Day_1.java
@@ -2,19 +2,16 @@ import java.util.*;
 
 public class Day_1 {
     public static void main(String[] args) {
-        System.out.println("Enter three numbers:");
+        System.out.println("Enter choice from 1 to 2");
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        
-        if(a>b&&a>c) {
-            System.out.println("a is greater than b and c");
-
-        } else if(b>a&&b>c) {
-            System.out.println("b id greater than a and c");
-        } else if(c>a&&c>b) {
-            System.out.println("c is greater than a and b");
+        int choice = sc.nextInt();
+        //int b = sc.nextInt();
+        //int c = sc.nextInt();
+        switch(choice) {
+            case 1 : System.out.println("hellow deep");
+            break;
+            case 2 : System.out.println("heelo prATIK");
+            break;
         }
 
     }
